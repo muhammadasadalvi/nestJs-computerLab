d8509b1f991a29e4bb4a8977146be4b613968939
asad alvi <asad.alvi@ideatolife.me>
2023-09-02 12:13:36 +0500

    create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    /*
+    ****One way****
+    repo: Repository<User>;
+
+    constructor(repo: Repository<User>) {
+        this.repo = repo;
+    }
+    */
+
+    /*
+    * @InjectRepository => it injects Users repository
+    * Repository<User> => generic type and dependency injection not work well with generic type so @InejctRepository is an aid
+    * private repo => it make repo accessible in class level
+    */
+    constructor(@InjectRepository(User) private repo: Repository<User>) {
+    }
+
+    create(email: string, password: string) {
+        const user = this.repo.create({
+            email: email,
+            password: password
+        });
+
+        return this.repo.save(user);
+    }
+}
