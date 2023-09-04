f6dbfa4e5964c3a1844c6d90b8a31e81e8664721
asad alvi <asad.alvi@ideatolife.me>
2023-09-04 13:07:39 +0500

    auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,17 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, Session, UseInterceptors } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 
 @Controller('auth')
 export class UsersController {
-    constructor(private userService: UsersService) { }
+    constructor(private userService: UsersService, private authService: AuthService) { }
 
-    @Post('/signup')
+    @Post('/create')
     createUsers(@Body() body: createUserDto) {
         this.userService.create(body.email, body.password)
     }
@@ -34,5 +35,21 @@ export class UsersController {
     @Patch(':id')
     updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
         return this.userService.update(parseInt(id), body)
+    }
+
+    @Post('/signup')
+    async signup(@Body() body: createUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+
+    }
+
+    @Post('/signin')
+    async signin(@Body() body: createUserDto, @Session() session: any) {
+        const user: any = await this.authService.signin(body.email, body.password);
+        if (user?.id) {
+            session.userId = user.id;
+        }
+
     }
 }
